8219faa5b150d067287925066378d1e8a00b51e9
GeorgesGil <103227730+GeorgesGil@users.noreply.github.com>
2023-04-15 12:38:29 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { pool } from "./db.js";
 import { PORT } from "./config.js";
-const cors = require("cors");
+//const cors = require("cors");
 
 const app = express();
 
