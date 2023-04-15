5e3b13028a22c2ffee3b2535bf26971dbd6f7e60
GeorgesGil <103227730+GeorgesGil@users.noreply.github.com>
2023-04-15 17:32:29 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -16,8 +16,8 @@ app.get("/", async (req, res) => {
 app.get("/call/:id", async (req, res) => {
   const id = req.params;
   console.log("este id es del backend: "+id)
-  const [result] = await pool.query("SELECT * FROM productos WHERE id = ?", [id]);
-  res.json(result);
+  const result = await pool.query("SELECT * FROM productos WHERE id = ?", [id]);
+  res.json(result[0]);
 });
 
 app.get("/crear", async (req, res) => {
