9d1bf07cb60e21911da4366311f5f80e64f1938f
GeorgesGil <103227730+GeorgesGil@users.noreply.github.com>
2023-04-15 11:30:47 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -5,18 +5,8 @@ import { PORT } from "./config.js";
 const app = express();
 
 app.get("/", async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM users");
+  const [rows] = await pool.query("SELECT * FROM productos");
   res.json(rows);
-});
-
-app.get("/ping", async (req, res) => {
-  const [result] = await pool.query(`SELECT "hello world" as RESULT`);
-  res.json(result[0]);
-});
-
-app.get("/create", async (req, res) => {
-  const result = await pool.query('INSERT INTO users(name) VALUES ("John")');
-  res.json(result);
 });
 
 app.get("/call", async (req, res) => {
